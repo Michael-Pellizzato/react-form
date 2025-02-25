99189b252008599a87e93a48dd38fdb4552c30a6
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-02-25 23:41:20 +0100

    creato articoli e punto di partenza

---
+++ src/data/articoli.js
@@ -0,0 +1,11 @@
+const articoliIniziali = [
+    "Portatile",
+    "Zaino",
+    "Orologio intelligente",
+    "Libro di programmazione",
+    "Cuffie Bluetooth",
+    "Smartphone",
+    "Mouse wireless"
+  ];
+
+ export default articoliIniziali  
